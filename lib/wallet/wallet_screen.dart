257bb10ff6cba1ea1c212d4e95f7ee8d4739a393
Leleblu <ladyrg123@gmail.com>
2026-09-23 11:56:00 -0500

    wallet_screen

---
+++ lib/wallet/wallet_screen.dart
@@ -0,0 +1,77 @@
+import "package:flutter/material.dart";
+
+class WalletScreen extends StatelessWidget {
+  const WalletScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mi billetera"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Saldo disponible:",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "\$100.000",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Acción para agregar fondos
+              },
+              child: const Text("Recargar saldo"),
+            ),
+            const SizedBox(height: 30),
+            
+            const Text(
+              "Ultimos movimientos:",
+              style: TextStyle(
+                fontSize: 20, 
+                fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 15),
+            
+            const ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Icon(Icons.arrow_downward),
+              ),
+              title: Text("Conferencia de Flutter"),
+              subtitle: Text("Pago de evento"),
+              trailing: Text("-\$20.000",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,)
+              ),
+            ),
+
+            const Divider(),
+
+            const ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Icon(Icons.arrow_upward),
+              ),
+              title: Text("Reembolso de evento"),
+              subtitle: Text("Evento cancelado"),
+              trailing: Text("+\$20.000",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,)
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
